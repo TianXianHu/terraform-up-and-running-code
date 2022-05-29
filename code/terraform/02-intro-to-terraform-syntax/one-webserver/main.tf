86f1d49c0f3dc9f792e777055834d133f89c5784
kasasagi <kasasagi@crc.test>
2022-05-29 19:05:20 +0300

    it's working. yay

--- code/terraform/02-intro-to-terraform-syntax/one-webserver/main.tf
+++ code/terraform/02-intro-to-terraform-syntax/one-webserver/main.tf
@@ -13,10 +13,33 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
+
+variable "security_group_name" {
+  description = "The name of my security group"
+  type        = string
+  default     = "terraform-example-instance"
+}
+
+resource "aws_security_group" "myinstances" {
+
+  name = var.security_group_name
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "terraform-example" 
+    }
+  
+}
+
+resource "aws_instance" "example_web_server" {
   ami                    = "ami-0fb653ca2d3203ac1"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  vpc_security_group_ids = [aws_security_group.myinstances.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -29,25 +52,7 @@ resource "aws_instance" "example" {
   }
 }
 
-resource "aws_security_group" "instance" {
-
-  name = var.security_group_name
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-variable "security_group_name" {
-  description = "The name of the security group"
-  type        = string
-  default     = "terraform-example-instance"
-}
-
 output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP of the Instance"
+  value       = aws_instance.example_web_server.public_ip
+  description = "The public IP of created web-server Instance"
 }
